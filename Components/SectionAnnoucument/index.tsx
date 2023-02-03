2dd9fb01f6555399e08090b124f170595625bd55
Myltiane <myltiane.aux@gmail.com>
2023-02-03 13:02:51 -0300

    feat: adicionando section announcements

--- Components/SectionAnnoucument/index.tsx
+++ Components/SectionAnnoucument/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
-import banner1 from '../../assets/images/banner-1.png'
-import banner2 from '../../assets/images/banner-2.png'
+import banner1 from '../../images/banner-1.png'
+import banner2 from '../../images/banner-2.png'
+import Image from 'next/image'
 export default function Announcements() {
   return (
     <S.Container>
       <div className="content">
         <div className="contentImg">
-          <img className="relogio" src={banner1} alt="Banner Promoção de Relógio" />
+          <Image className="relogio" src={banner1} alt="Banner Promoção de Relógio" />
         </div>
         <div className="contentImg">
-          <img className="mini" src={banner2} alt="Banner Promoção Minimalista" />
+          <Image className="mini" src={banner2} alt="Banner Promoção Minimalista" />
         </div>
       </div>
     </S.Container>
